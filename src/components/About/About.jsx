477d4e421dca0ba67a5ee756298b2e08823fcea4
Beerlogich <beerlogich@gmail.com>
2024-08-14 16:02:46 -0400

    Changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 const jobs = [
   {
-    title: "Self Employed",
-    type: "Full-time",
-    company: "Lenape Pizza",
-    location: "PA, United States",
+    title: "Frontend Web Developer",
+    type: "Freelance",
+    company: "UpWork",
+    location: "Remote, United States",
     time: "May 2023 - Present",
   },
   {
@@ -17,42 +17,27 @@ const jobs = [
     location: "CA, United States",
     time: "Aug 2023 - Dec 2023",
   },
-  {
-    title: "Relocation",
-    type: "Career Break",
-    company: "Relocation to the USA",
-    location: "Russia - USA",
-    time: "Sep 2022 - Aug 2023",
-  },
+
   {
     title: "Frontend Web Developer",
     type: "Full-time",
     company: "SЕОТLТ",
-    location: "Russia",
+    location: "",
     time: "Sep 2020 - Sep 2022",
-  },
-  {
-    title: "Detective",
-    type: "Full-time",
-    company: "Police",
-    location: "Russia",
-    time: "Mar 2012 - Sep 2020",
   },
 ];
 
 export const About = () => {
   return (
     <div className="flex flex-col w-full flex-auto">
-      {
-        <Link
+      {/*         <Link
           to="/files/FrontendResume.pdf"
           target="_blank"
           download
           className="max-w-[285px] mb-[40px] text-[20px] text-link-color sm:mb-[20px] underline-offset-1 border-b-[1px] border-solid border-black"
         >
           My Resume(click to download)
-        </Link>
-      }
+        </Link> */}
       <h2>About Me</h2>
       <p className="max-w-[700px] mb-[40px] text-[20px] text-link-color sm:mb-[20px]">
         I'm frontend developer with experience working with HTML, CSS/SCSS,
